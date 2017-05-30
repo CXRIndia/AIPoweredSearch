a290d50d60791ea6f4424d76df574c9308c3abd2
Madhurjya Pegu <migom6@gmail.com>
2017-05-30 18:35:57 +0530

    added spell checker and autocompletion

---
+++ db.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('searchtool', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  })
